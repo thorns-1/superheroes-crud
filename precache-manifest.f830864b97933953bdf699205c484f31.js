840ded4762aa62d18e41d5ffec183d2716546641
IvanKryuchkov <55559380+IvanKryuchkov@users.noreply.github.com>
2020-07-14 15:28:21 +0300

    Updates

---
+++ precache-manifest.f830864b97933953bdf699205c484f31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ee101ae3f53cefabb026793b5202ee",
+    "url": "/superheroes-crud/index.html"
+  },
+  {
+    "revision": "a858ed9d4fb25b2085f8",
+    "url": "/superheroes-crud/static/css/main.f24951f0.chunk.css"
+  },
+  {
+    "revision": "8b8e2253fa751dda4c8b",
+    "url": "/superheroes-crud/static/js/2.e578396a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/superheroes-crud/static/js/2.e578396a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a858ed9d4fb25b2085f8",
+    "url": "/superheroes-crud/static/js/main.725d7c34.chunk.js"
+  },
+  {
+    "revision": "c9f5417deef77555d8d9",
+    "url": "/superheroes-crud/static/js/runtime-main.64ad5b2a.js"
+  }
+]);
